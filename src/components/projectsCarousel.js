4ff7726d862eb87390a6ec0dc906f1a9c7bfa3fb
ddlan <L.danny18725@gmail.com>
2018-12-27 19:44:32 -0500

    Rearranged carousel

--- src/components/projectsCarousel.js
+++ src/components/projectsCarousel.js
@@ -11,17 +11,8 @@ class ProjectsCarousel extends Component {
         return (
           <Carousel>
             <Carousel.Item>
-              <img width={2500} height={500} alt="NightBall" src={require("../Assets/img/nightcover.png")} />
-              <Link to="/Projects/NightBall">
-                <Carousel.Caption style={capStyle}>
-                  <h3>NightBall</h3>
-                  <p>Groove to the beat while you match colours in this arcade game! (Swift)</p>
-                </Carousel.Caption>
-              </Link>
-              </Carousel.Item>
-            <Carousel.Item>
               <img width={2500} height={500} alt="BetterPD/EmployAbility" src={require("../Assets/img/loologo.png")} />
-              <Link to="/Projects/NightBall">
+              <Link to="/Projects/BetterPD">
                 <Carousel.Caption style={capStyle}>
                   <h3>BetterPD/EmployAbility</h3>
                   <p>Job search preparation for the busy university student. (Python, Flask, MySQL)</p>
@@ -29,8 +20,17 @@ class ProjectsCarousel extends Component {
               </Link>
             </Carousel.Item>
             <Carousel.Item>
-              <img width={2500} height={500} alt="Altocumulus Industries" src={require("../Assets/img/altocumulus.jpg")} />
+              <img width={2500} height={500} alt="NightBall" src={require("../Assets/img/nightcover.png")} />
               <Link to="/Projects/NightBall">
+                <Carousel.Caption style={capStyle}>
+                  <h3>NightBall</h3>
+                  <p>Groove to the beat while you match colours in this arcade game! (Swift)</p>
+                </Carousel.Caption>
+              </Link>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img width={2500} height={500} alt="Altocumulus Industries" src={require("../Assets/img/altocumulus.jpg")} />
+              <Link to="/Projects/Altocumulus">
                 <Carousel.Caption style={capStyle}>
                   <h3>Altocumulus Industries</h3>
                   <p>Machine Learning Facial Recognition to find friends in a crowd. (Python/AWS)</p>
