078387e124923111d230741cde3d5bf7e0581260
ddlan <L.danny18725@gmail.com>
2019-07-11 10:50:39 -0400

    keep heroku up

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+var http = require("http");
+setInterval(function() {
+    http.get("http://www.dannylan.me");
+}, 300000); // every 5 minutes (300000)
